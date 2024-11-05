9d44f1a0b572506a935ac1040c7066469e750b80
Juliankey321Dev <juliankey321yt@gmail.com>
2024-11-05 18:07:20 +0100

    Update XStuffTool.java

--- XStuffTool.java
+++ XStuffTool.java
@@ -1,6 +1,8 @@
 package de.juliankey321.xStuffTool;
 
+import de.juliankey321.xStuffTool.Listeners.Listener;
 import de.juliankey321.xStuffTool.commands.Fly;
+import de.juliankey321.xStuffTool.commands.Freeze;
 import de.juliankey321.xStuffTool.commands.Nick;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -13,6 +15,9 @@ public final class XStuffTool extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes(ChatColor.COLOR_CHAR, "§8[§6PREFIX§8] §aPlugin is now enabled!"));
         getCommand("fly").setExecutor(new Fly());
         getCommand("nick").setExecutor(new Nick());
+        getCommand("freeze").setExecutor(new Freeze());
+
+        Bukkit.getPluginManager().registerEvents(new Listener(), this);
     }
 
     @Override
